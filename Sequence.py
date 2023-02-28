9377aa2b3719a9b4d109511b48764fcabf36e02e
Luis David Huante <69726163+LuisDHuante@users.noreply.github.com>
2023-02-28 11:53:10 -0600

    Update Sequence.py

--- Sequence.py
+++ Sequence.py
@@ -1,5 +1,6 @@
 def sequence(n):
   sequence = ["\\,frac{1}{10^"+f"{i}"+"}" for i in range(1,n+1)]
-  return sequence
+   evaluations = [1/10**i for i in range(n)]
+  return sequence, evaluations
 
 print(sequence(int(input("Choose a number: "))))
